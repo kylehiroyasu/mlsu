e0467e671fcf2a5f344d27a94748f9947273e199
Kyle Hiroyasu <kyle.hiroyasu@valiton.com>
2018-04-23 16:38:13 +0200

    finished with the regularized versions of cost and gradient functions

--- week_9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ week_9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,11 +41,11 @@ Theta_grad = zeros(size(Theta));
 %  i = 3 movies
 % j = 4 users
 %
-J = sum(sum((((X * Theta') - Y) .* R).^2, 2))/2;
-X
-X_grad =  (((X * Theta') - Y) .* R) * Theta
-Theta
-Theta_grad = (((X * Theta') - Y) .* R)' * X
+J = sum(sum((((X * Theta') - Y) .* R).^2, 2))/2 + (lambda / 2 * sum(sum(Theta.^2, 2))) + (lambda / 2 * sum(sum(X.^2, 2))); 
+
+X_grad =  (((X * Theta') - Y) .* R) * Theta .+ (lambda .* X);
+
+Theta_grad = (((X * Theta') - Y) .* R)' * X .+ (lambda .* Theta);
 
 
 
